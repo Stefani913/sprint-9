804f96f3fda887c4b83f0338470dc009f4ca48b7
Stefani <stefaniisaeva@yandex.ru>
2025-11-23 23:55:51 +0300

    first

--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/Yandex-Practicum/sprint9-final
 
 go 1.24.1
+
+require (
+	github.com/stretchr/testify v1.8.4
+//github.com/davecgh/go-spew v1.1.1 // ispewndirect
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- main_test.go
+++ main_test.go
@@ -1,3 +1,46 @@
 package main
 
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
 // Пишите тесты в этом файле
+func TestGenerateRandomElementsBySize(t *testing.T) {
+	size := 5
+	slice := generateRandomElements(size)
+
+	assert.Equal(t, size, len(slice))
+	require.NotEmpty(t, slice)
+}
+
+func TestNotGenerateRandomElementsBySize(t *testing.T) {
+	size := 0
+	slice := generateRandomElements(size)
+
+	assert.Equal(t, size, len(slice))
+	require.Empty(t, slice)
+}
+
+func TestReturnMax(t *testing.T) {
+	var data = []int{1, 2, 3, 4, 5}
+	result := maximum(data)
+
+	assert.Equal(t, 5, result)
+}
+
+func TestReturnMaxSingleElement(t *testing.T) {
+	var data = []int{1}
+	result := maximum(data)
+
+	assert.Equal(t, 1, result)
+}
+
+func TestMaxForEmptySlice(t *testing.T) {
+	data := make([]int, 0)
+	result := maximum(data)
+
+	assert.Equal(t, 0, result)
+}
