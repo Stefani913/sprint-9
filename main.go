9653c9452b510158bdf772a68a721f02f16cb758
Stefani <stefaniisaeva@yandex.ru>
2025-11-24 22:01:47 +0300

    fixed

--- main.go
+++ main.go
@@ -29,33 +29,42 @@ func generateRandomElements(size int) []int {
 
 // maximum returns the maximum number of elements.
 func maximum(data []int) int {
-	if len(data) < 1 {
-		fmt.Println("Длина слайса data не должна быть меньше 1.")
+	if len(data) == 0 {
+		fmt.Println("Длина слайса data не должна равняться 0.")
 		return 0
 	}
 	return slices.Max(data)
 }
 
 // maxChunks returns the maximum number of elements in a chunks.
-func maxChunks(data []int) int {
-	var wg sync.WaitGroup
-	size := len(data)
-	chunkSize := size / CHUNKS
-	maxValues := make([]int, CHUNKS)
-	index := 0
+func maxChunks(data []int) (int, error) {
 
-	wg.Add(CHUNKS)
-
-	for chunk := range slices.Chunk(data, chunkSize) {
-		go func() {
-			defer wg.Done()
-			maxValues[index] = maximum(chunk)
-			index++
-		}()
+	if len(data) == 0 {
+		fmt.Println("Длина слайса data не должна равняться 0.")
+		return 0, nil
 	}
 
+	size := len(data)
+	chunkSize := size / CHUNKS
+
+	// добавляем проверку, тк в противном случае slices.Chunk() выбросит panic
+	if chunkSize < 1 {
+		err := fmt.Errorf("рассчитанное значение чанка не должно быть меньше 1")
+		return 0, err
+	}
+
+	maxValues := make([]int, 0)
+	var wg sync.WaitGroup
+
+	for chunk := range slices.Chunk(data, chunkSize) {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			maxValues = append(maxValues, maximum(chunk))
+		}()
+	}
 	wg.Wait()
-	return maximum(maxValues)
+	return maximum(maxValues), nil
 }
 
 func main() {
@@ -65,17 +74,18 @@ func main() {
 	fmt.Println("Ищем максимальное значение в один поток")
 	start := time.Now()
 	max := maximum(randNumSlice)
-	end := time.Now()
-	elapsed := end.Sub(start)
+	elapsed := time.Since(start)
 
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
 
 	fmt.Printf("Ищем максимальное значение в %d потоков", CHUNKS)
 	// ваш код здесь
 	start = time.Now()
-	max = maxChunks(randNumSlice)
-	end = time.Now()
-	elapsed = end.Sub(start)
+	max, err := maxChunks(randNumSlice)
+	if err != nil {
+		fmt.Println("Возникла ошибка при поиске максимума: ", err)
+	}
+	elapsed = time.Since(start)
 
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
 }
--- main_test.go
+++ main_test.go
@@ -5,42 +5,140 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 )
 
-// Пишите тесты в этом файле
-func TestGenerateRandomElementsBySize(t *testing.T) {
-	size := 5
-	slice := generateRandomElements(size)
-
-	assert.Equal(t, size, len(slice))
-	require.NotEmpty(t, slice)
+type MaxValueTestSuite struct {
+	suite.Suite
 }
 
-func TestNotGenerateRandomElementsBySize(t *testing.T) {
-	size := 0
-	slice := generateRandomElements(size)
-
-	assert.Equal(t, size, len(slice))
-	require.Empty(t, slice)
+func TestMaxValueSuite(t *testing.T) {
+	suite.Run(t, new(MaxValueTestSuite))
 }
 
-func TestReturnMax(t *testing.T) {
-	var data = []int{1, 2, 3, 4, 5}
-	result := maximum(data)
+func (suite *MaxValueTestSuite) TestGenerateRandomElements() {
+	tests := []struct {
+		name        string
+		input       int
+		factIsEmpty bool
+	}{
+		{
+			name:        "Создание массива для нескольки значений",
+			input:       5,
+			factIsEmpty: false,
+		},
+		{
+			name:        "Создание массива для одного значения",
+			input:       1,
+			factIsEmpty: false,
+		},
+		{
+			name:        "Создание пустого массива, если передан 0",
+			input:       0,
+			factIsEmpty: true,
+		},
+	}
 
-	assert.Equal(t, 5, result)
+	for _, tt := range tests {
+		suite.Run(tt.name, func() {
+			slice := generateRandomElements(tt.input)
+			sliceSize := len(slice)
+
+			if tt.factIsEmpty {
+				require.Empty(suite.T(), slice)
+
+			} else {
+				require.NotEmpty(suite.T(), slice)
+			}
+
+			assert.Equal(suite.T(), tt.input, sliceSize, "generateRandomElements() возвращает значение %d. Ожидаемое %d", tt.input, sliceSize)
+		})
+	}
 }
 
-func TestReturnMaxSingleElement(t *testing.T) {
-	var data = []int{1}
-	result := maximum(data)
+func (suite *MaxValueTestSuite) TestFindingMaxElement() {
+	tests := []struct {
+		name     string
+		input    []int
+		expected int
+	}{
+		{
+			name:     "Поиск максимума в слайсе из нескольких одинаковых значений",
+			input:    []int{1, 1, 1, 1, 1},
+			expected: 1,
+		},
+		{
+			name:     "Поиск максимума в слайсе из нескольких разных значений",
+			input:    []int{1, 2, 3, 4, 5, 13},
+			expected: 13,
+		},
+		{
+			name:     "Поиск максимума в слайсе из одного значения",
+			input:    []int{9},
+			expected: 9,
+		},
+		{
+			name:     "Поиск максимума в путом слайсе",
+			input:    []int{},
+			expected: 0,
+		},
+	}
 
-	assert.Equal(t, 1, result)
+	for _, tt := range tests {
+		suite.Run(tt.name, func() {
+			maxNumber := maximum(tt.input)
+
+			assert.Equal(suite.T(), tt.expected, maxNumber, "maximum() возвращает значение %d. Ожидаемое %d", tt.expected, maxNumber)
+		})
+	}
 }
+func (suite *MaxValueTestSuite) TestMaxElementsInChunks() {
+	tests := []struct {
+		name      string
+		input     []int
+		expectErr bool
+		expected  int
+	}{
+		{
+			name:      "Поиск максимума в слайсе из нескольких значений, количество которых равно 8 (1 чанк)",
+			input:     []int{4, 3, 0, 1, 1, 2, 1, 5},
+			expectErr: false,
+			expected:  5,
+		},
+		{
+			name:      "Поиск максимума в слайсе из нескольких значений, количество которых больше 8 (2 чанка)",
+			input:     []int{1, 1, 1, 1, 1, 2, 4, 1, 51, 22, 114141, 22, 1, 0, 44, 876},
+			expectErr: false,
+			expected:  114141,
+		},
+		{
+			name:      "Поиск максимума в слайсе из нескольких значений, количество которых меньше 8",
+			input:     []int{9, 13},
+			expectErr: true,
+		},
+		{
+			name:      "Поиск максимума в слайсе из одного значения",
+			input:     []int{9},
+			expectErr: true,
+		},
+		{
+			name:      "Поиск максимума в путом слайсе",
+			input:     []int{},
+			expectErr: false,
+			expected:  0,
+		},
+	}
 
-func TestMaxForEmptySlice(t *testing.T) {
-	data := make([]int, 0)
-	result := maximum(data)
+	for _, tt := range tests {
+		suite.Run(tt.name, func() {
+			maxNumber, err := maxChunks(tt.input)
 
-	assert.Equal(t, 0, result)
+			if tt.expectErr {
+				assert.Error(suite.T(), err, "Ожидалась ошибка %q и мы ее получили", err)
+			} else {
+				assert.NoError(suite.T(), err, " Неожиданных ошибок не возникло: %q", err)
+				assert.Equal(suite.T(), tt.expected, maxNumber, "maximum() возвращает значение %d. Ожидаемое %d", maxNumber, tt.expected)
+			}
+		})
+	}
 }
